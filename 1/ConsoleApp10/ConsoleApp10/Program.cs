13dbdaad89a05444a6bd5ff570291dab6a02fc3a
ytrbjbytbtr <52233543+ytrbjbytbtr@users.noreply.github.com>
2023-04-11 13:35:11 +0300

    Add files via upload

---
+++ 1/ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Method();
+            }
+            catch (Exception ex) when (Check(ex))
+            {
+                Console.Write('B');
+            }
+        }
+
+        static void Method()
+        {
+            try { throw new Exception(); }
+            finally { Console.Write('A'); }
+        }
+
+        static bool Check(Exception ex)
+        {
+            Console.Write("C");
+            return true;
+        }
+    }
+}
---
+++ 2/ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "Петров", "Гусев", "Антоненко", "Данилков", "Афанасьев", "Смирнов" };
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                string cur = array[i];
+                int j = i;
+                while (j > 0 && String.Compare(cur, array[j - 1]) < 0)
+                {
+                    array[j] = array[j - 1];
+                    j--;
+                }
+                array[j] = cur;
+            }
+            Console.WriteLine(String.Join(" ", array));
+        }
+    }
+}
